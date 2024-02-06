a1eeb267bc8d556702d9fbea3bc642882cfee64a
Michael Owuor <michael.owuor@gmail.com>
2024-02-06 10:02:27 +0300

    feat: add theme toggle block to navigation footer and update component exports

--- components/blocks/navigation-footer-block.tsx
+++ components/blocks/navigation-footer-block.tsx
@@ -1,3 +1,10 @@
+import { ThemeToggleBlock } from "@/components/index";
+
 export const NavigationFooterBlock = () => {
-	return <div>Navigation Footer Block</div>;
+	return (
+		<div>
+			Navigation Footer Block
+			<ThemeToggleBlock />
+		</div>
+	);
 };
--- components/index.ts
+++ components/index.ts
@@ -1,11 +1,29 @@
 /* component blocks */
-import { NavigationFooterBlock } from '@/components/blocks/navigation-footer-block';
-import { NavigationHeaderBlock } from '@/components/blocks/navigation-header-block';
+import { NavigationFooterBlock } from "@/components/blocks/navigation-footer-block";
+import { NavigationHeaderBlock } from "@/components/blocks/navigation-header-block";
+import { ThemeToggleBlock } from "@/components/blocks/theme-toggle-block";
 
 /* notification components */
 
 /* shadcn ui elements */
+import { Button } from "@/components/ui/button";
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 /* component widgets */
 
-export { NavigationFooterBlock, NavigationHeaderBlock };
+/* export components */
+export {
+	NavigationFooterBlock,
+	NavigationHeaderBlock,
+	ThemeToggleBlock,
+	Button,
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuTrigger,
+};
