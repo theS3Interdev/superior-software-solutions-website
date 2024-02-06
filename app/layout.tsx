608cb7c0b797f8630542dbd7a7d232a29b474738
Michael Owuor <michael.owuor@gmail.com>
2024-02-06 05:58:58 +0300

    added theme provider to layout and shadcn components

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,27 @@
-import { ReactNode } from 'react';
-import type { Metadata } from 'next';
-import { Analytics } from '@vercel/analytics/react';
-import { SpeedInsights } from '@vercel/speed-insights/next';
+import { ReactNode } from "react";
+import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
-import { RecaptchaProvider } from '@/lib/providers/google-recaptcha-provider';
-import { ToastProvider } from '@/lib/providers/toast-provider';
+import { RecaptchaProvider } from "@/lib/providers/google-recaptcha-provider";
+import { ThemeProvider } from "@/lib/providers/theme-provider";
+import { ToastProvider } from "@/lib/providers/toast-provider";
 
-import '@/app/styles/globals.css';
+import "@/app/styles/globals.css";
 
 export const revalidate = 60;
 
 /* get the url depending on the environment */
 const absoluteUrl =
-	process.env.NODE_ENV === 'production'
+	process.env.NODE_ENV === "production"
 		? process.env.NEXT_PUBLIC_PRODUCTION_URL!
 		: process.env.NEXT_PUBLIC_DEVELOPMENT_URL!;
 
 export const metadata: Metadata = {
 	metadataBase: new URL(absoluteUrl),
 	title: {
-		default: 'Superior Software Solutions',
-		template: '%s | Superior Software Solutions',
+		default: "Superior Software Solutions",
+		template: "%s | Superior Software Solutions",
 	},
 	description:
 		"Kenya's premier software design studio. Superior Software Solutions: websites, webapps, success stories. Let's craft yours.",
@@ -34,12 +35,19 @@ const RootLayout = ({ children }: RootLayoutProps) => {
 	return (
 		<html lang="en">
 			<body className="scroll-smooth font-sans antialiased">
-				<RecaptchaProvider>
-					{children}
-					<Analytics />
-					<SpeedInsights />
-					<ToastProvider />
-				</RecaptchaProvider>
+				<ThemeProvider
+					attribute="class"
+					defaultTheme="system"
+					enableSystem
+					disableTransitionOnChange
+				>
+					<RecaptchaProvider>
+						{children}
+						<Analytics />
+						<SpeedInsights />
+						<ToastProvider />
+					</RecaptchaProvider>
+				</ThemeProvider>
 			</body>
 		</html>
 	);
