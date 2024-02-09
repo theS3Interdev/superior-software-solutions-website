c413cdbd870c6b05a10561f0753add1263c04ce6
Michael Owuor <michael.owuor@gmail.com>
2024-02-09 11:00:48 +0300

    feat: add home page metadata generation
    This commit adds the functionality to generate metadata for the home page. The `generateMetadata` function retrieves the necessary data from the `getHomePage` function and returns a `Metadata` object with the appropriate values. This will be used for SEO purposes.
    
    fix: Update primary color in global styles
    
    The primary color in the global styles has been updated to match the new design requirements. The HSL values have been adjusted to achieve the desired color.
    
    refactor: Update font family in Tailwind config
    
    The font family in the Tailwind config has been updated to use "Poppins" as the primary sans-serif font. This change aligns with the design specifications and ensures consistency across components and pages.

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,10 +1,55 @@
+import { Metadata } from "next";
+import { getCldOgImageUrl } from "next-cloudinary";
+
+import { getHomePage } from "@/lib/data/read/index";
+
 import { HomeContentWidget, HomeHeroWidget } from "@/components/index";
 
-const HomePage = () => {
+type HomePageProps = {
+	pageTitle: string;
+	pageMetaDescription: string;
+	heroActionBlock: {
+		content: {
+			content: { header: { title: string; subtitle: string } };
+			image: { public_id: string };
+		};
+		links: {
+			label: string;
+			url: string;
+		}[];
+	};
+};
+
+export async function generateMetadata(): Promise<Metadata> {
+	const { pageTitle, pageMetaDescription, heroActionBlock }: HomePageProps =
+		await getHomePage();
+
+	return {
+		title: pageTitle,
+		description: pageMetaDescription,
+		openGraph: {
+			type: "article",
+			images: [
+				{
+					width: 1200,
+					height: 630,
+					url: getCldOgImageUrl({
+						src: heroActionBlock.content.image.public_id,
+						format: "jpg",
+					}),
+				},
+			],
+		},
+	};
+}
+
+const HomePage = async () => {
+	const { heroActionBlock }: HomePageProps = await getHomePage();
+
 	return (
 		<article className="mt-24 space-y-8">
 			<section id="hero">
-				<HomeHeroWidget />
+				<HomeHeroWidget heroActionBlock={heroActionBlock} />
 			</section>
 
 			<section id="content">
