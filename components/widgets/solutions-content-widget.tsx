3714b512472c3ba72f810a1127f28395322885a0
Michael Owuor <michael.owuor@gmail.com>
2024-02-20 16:32:44 +0300

    feat(solutions): add solutions page components and data retrieval
    This commit adds the necessary components and functions to display the solutions page. It also includes a new function `getSolutionsPage` that retrieves the required data for the solutions page from the backend.
    
    The following files were modified:
    - app/(root)/(solutions)/solutions/page.tsx
    - components/widgets/blogs-content-widget.tsx
    - components/widgets/solutions-content-widget.tsx
    - components/widgets/solutions-hero-widget.tsx
    - lib/data/operations/queries/index.ts
    - lib/data/read/index.ts

--- components/widgets/solutions-content-widget.tsx
+++ components/widgets/solutions-content-widget.tsx
@@ -1,12 +1,39 @@
 import { Container } from "@/components/container";
+import { CallToActionBlock, Separator } from "@/components/index";
 
-type SolutionsContentWidgetProps = {};
+type SolutionsContentWidgetProps = {
+	callToAction: {
+		image: { public_id: string };
+		title: string;
+		content: {
+			html: string;
+		};
+		link: {
+			label: string;
+			url: string;
+		};
+	};
+};
 
-export const SolutionsContentWidget = ({}: SolutionsContentWidgetProps) => {
+export const SolutionsContentWidget = ({
+	callToAction,
+}: SolutionsContentWidgetProps) => {
 	return (
 		<Container>
 			<div className="py-8">
-				<div className="space-y-8">Solutions Content Widget</div>
+				<div className="space-y-8">
+					<Separator className="my-8" />
+
+					<div>
+						<CallToActionBlock
+							image={callToAction.image.public_id}
+							title={callToAction.title}
+							content={callToAction.content.html}
+							label={callToAction.link.label}
+							url={callToAction.link.url}
+						/>
+					</div>
+				</div>
 			</div>
 		</Container>
 	);
--- components/widgets/solutions-hero-widget.tsx
+++ components/widgets/solutions-hero-widget.tsx
@@ -1,5 +1,33 @@
-type SolutionsHeroWidgetProps = {};
+import { HeroDisplayAlternateBlock } from "@/components/index";
 
-export const SolutionsHeroWidget = ({}: SolutionsHeroWidgetProps) => {
-	return <div className="mx-1">Solutions Hero Widget</div>;
+type SolutionsHeroWidgetProps = {
+	heroActionBlock: {
+		content: {
+			content: { header: { title: string } };
+			image: { public_id: string };
+		};
+	};
+	pasBlock: {
+		header: {
+			content: {
+				header: { title: string; subtitle: string };
+			};
+		};
+	};
+};
+
+export const SolutionsHeroWidget = ({
+	heroActionBlock,
+	pasBlock,
+}: SolutionsHeroWidgetProps) => {
+	return (
+		<div className="mx-1">
+			<HeroDisplayAlternateBlock
+				image={heroActionBlock.content.image.public_id}
+				title={heroActionBlock.content.content.header.title}
+				subtitleHT={pasBlock.header.content.header.title}
+				subtitleHS={pasBlock.header.content.header.subtitle}
+			/>
+		</div>
+	);
 };
