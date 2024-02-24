e2a4962aceb9a06cebedc620a0a73fbad1dbb556
Michael Owuor <michael.owuor@gmail.com>
2024-02-24 18:07:32 +0300

    feat: refactor about-content-widget to use PASJumbotronBlock
    This commit refactors the AboutContentWidget component to use the newly created PASJumbotronBlock component. The code changes include removing unnecessary imports and replacing the previous header, image, and content display blocks with the new PASJumbotronBlock.

--- components/widgets/about-content-widget.tsx
+++ components/widgets/about-content-widget.tsx
@@ -1,9 +1,7 @@
 import { Container } from "@/components/container";
 import {
 	CallToActionBlock,
-	ContentDisplayBlock,
-	HeaderDisplayBlock,
-	ImageDisplayBlock,
+	PASJumbotronBlock,
 	Separator,
 } from "@/components/index";
 
@@ -53,58 +51,12 @@ export const AboutContentWidget = ({
 		<Container>
 			<div className="py-8">
 				<div className="space-y-8">
-					<div className="w-full space-y-5 rounded-lg border bg-secondary p-5">
-						<div>
-							<HeaderDisplayBlock
-								title="Bridging Success"
-								subtitle="The Superior Software Solutions story"
-							/>
-						</div>
-
-						<div className="flex flex-col gap-5 rounded-lg lg:flex-row">
-							<div className="px-1 lg:w-1/2">
-								<div className="relative h-64 w-full lg:h-72">
-									<ImageDisplayBlock
-										imageSrc={pasBlock.header.image.public_id}
-										imageAlt="About Image"
-									/>
-								</div>
-							</div>
-
-							<div className="space-y-5 px-1 lg:w-1/2">
-								<div>
-									<HeaderDisplayBlock
-										title="Michael Caesar"
-										subtitle="Founder @Superior Software Solutions"
-									/>
-								</div>
-
-								<div>
-									<ContentDisplayBlock
-										content={pasBlock.header.content.content.html}
-									/>
-								</div>
-							</div>
-						</div>
-
-						<div className="grid grid-cols-1 gap-5 lg:grid-cols-3">
-							{pasBlock.list.map((item, index) => (
-								<div
-									key={index}
-									className="overflow-hidden rounded-lg border-2 bg-secondary"
-								>
-									<div className="p-5">
-										<h3 className="text-lg font-semibold tracking-wide">
-											{item.content.header.title}
-										</h3>
-
-										<Separator className="my-3" />
-
-										<ContentDisplayBlock content={item.content.content.html} />
-									</div>
-								</div>
-							))}
-						</div>
+					<div>
+						<PASJumbotronBlock
+							image={pasBlock.header.image.public_id}
+							content={pasBlock.header.content.content.html}
+							pasBlock={pasBlock}
+						/>
 					</div>
 
 					<Separator className="my-8" />
